2e33e43385c086ce3f4fca6785c6da6c973a5757
Matthew Oballe <oballematt@gmail.com>
2021-07-13 08:30:26 -0500

    changing bcrypt to bcryptjs to try to fix deploy errors.

--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 'use strict';
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const {
   Model
 } = require('sequelize');
--- passportConfig.js
+++ passportConfig.js
@@ -1,6 +1,6 @@
 const LocalStrategy = require("passport-local").Strategy;
 const  pool  = require("./config/dbConfig");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 function initialize(passport) {
   console.log("Initialized");
