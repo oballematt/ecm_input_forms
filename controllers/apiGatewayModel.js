35cf0b6a5ed778a0d3430fc1b292ea17ff3d02fe
Matthew Oballe <oballematt@gmail.com>
2021-11-16 11:18:24 -0600

    adding conditional.

--- controllers/apiGatewayModel.js
+++ controllers/apiGatewayModel.js
@@ -6,12 +6,19 @@ module.exports = {
         try {
             const config = {
                 headers: {
-                    'authorizationToken': '' 
+                    'authorizationToken': process.env.TOKEN
                 }
             }
             const model = process.env.API_URL + `building_number=${buildingNumber}&commodity_tag=${commodity}&meter=${meter}&train_start=${trainStart}&train_end=${trainEnd}&analysis_start=${analysisStart}&analysis_end=${analysisEnd}`
             const response = await axios.get(model, config)
-            return res.json(response.data)
+            console.log(req.user.email)
+            if (response === 'Request failed with status code 504') {
+                return res.json('not wokring')
+            } else {
+                return res.json(response.data)
+            }
+            
+
 
         } catch (error) {
             console.error(error.message)
